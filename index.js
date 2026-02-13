bd533eb67ec54426ffe03dd037e37ae43efdaa05
Raisa Tabassum <raisatabassume@gmail.com>
2026-02-13 18:42:50 +0600

    added purchase model and delete model route and service key,verifyToken

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 require("dotenv").config();
+const admin = require("firebase-admin");
+const serviceAccount = require("./ai-model-inventory-manager-serviceKey.json");
 app.use(cors());
 app.use(express.json());
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wsasknz.mongodb.net/?appName=Cluster0`;
@@ -18,14 +24,36 @@ const client = new MongoClient(uri, {
   },
 });
 
+const verifyToken = async (req, res, next) => {
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).send({
+      message: "Unauthorized Access. Token Not Found",
+    });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = await admin.auth().verifyIdToken(token);
+    req.decoded = decoded;
+    next();
+  } catch (error) {
+    return res.status(401).send({
+      message: "Unauthorized Access.",
+    });
+  }
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const db = client.db("ai-models-db");
-    const modelsCollection = db.collection("all-models");
+    const modelsCollection = db.collection("models");
 
-    app.get("/all-models", async (req, res) => {
+    app.get("/models", async (req, res) => {
       const models = await modelsCollection.find().toArray();
       res.send(models);
     });
@@ -50,6 +78,52 @@ async function run() {
       const model = req.body;
       const result = await modelsCollection.insertOne(model);
       res.send(result);
+    });
+
+    app.delete("/models/:id", verifyToken, async (req, res) => {
+      const id = req.params.id;
+
+      const model = await modelsCollection.findOne({ _id: new ObjectId(id) });
+
+      if (!model) {
+        return res.status(404).send({ message: "Model not found" });
+      }
+
+      if (model.createdBy !== req.decoded.email) {
+        return res.status(403).send({
+          message: "Forbidden! You are not the creator.",
+        });
+      }
+      const result = await modelsCollection.deleteOne({
+        _id: new ObjectId(id),
+      });
+      res.send({
+        success: true,
+        result,
+      });
+    });
+
+    const purchaseCollection = db.collection("purchases");
+
+    app.post("/purchase/:id", verifyToken, async (req, res) => {
+      const data = req.body;
+      const id = req.params.id;
+
+      const PurchaseResult = await purchaseCollection.insertOne(data);
+
+      // increment
+      const filter = { _id: new ObjectId(id) };
+      const update = {
+        $inc: {
+          purchased: 1,
+        },
+      };
+      const purchaseCount = await modelsCollection.updateOne(filter, update);
+      res.send({
+        success: true,
+        PurchaseResult,
+        purchaseCount,
+      });
     });
 
     // Send a ping to confirm a successful connection
